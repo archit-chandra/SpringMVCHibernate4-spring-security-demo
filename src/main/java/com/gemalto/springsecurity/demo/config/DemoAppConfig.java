cb222fca3a4297207c8c6b94947d9ff9d8a8e880
DESKTOP-BL7US23\architbiet <architbiet@gmail.com>
2018-04-14 23:59:37 +0200

    defined datasource in spring configuration

--- src/main/java/com/gemalto/springsecurity/demo/config/DemoAppConfig.java
+++ src/main/java/com/gemalto/springsecurity/demo/config/DemoAppConfig.java
@@ -1,15 +1,72 @@
 package com.gemalto.springsecurity.demo.config;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.util.logging.Logger;
 
 @Configuration // replaces spring config xml
 @EnableWebMvc  // equavalent of <mvc:annotation-driven />
 @ComponentScan(basePackages = "com.gemalto.springsecurity.demo")
+@PropertySource("classpath:persistence-mysql.properties")
 public class DemoAppConfig {
+
+    // setup a variable to hold the data read from properties file
+    @Autowired
+    private Environment env;
+
+    // setup a logger for diagnostics
+    private Logger logger = Logger.getLogger(getClass().getName());
+
+    // define a bean for datasource
+    @Bean
+    public DataSource securityDataSource() {
+
+        // create connection pool
+        ComboPooledDataSource securityDataSource = new ComboPooledDataSource();
+
+        // set the jdbc driver class
+        try {
+            securityDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+        } catch (PropertyVetoException e) {
+            //e.printStackTrace();
+            throw new RuntimeException(e);
+        }
+
+        //log the connection props
+        // just to make sure that we are really reading from properties file
+        logger.info(">>> jdbc.url = " + env.getProperty("jdbc.url"));
+        logger.info(">>> jdbc.user = " + env.getProperty("jdbc.user"));
+
+        // set database connection props
+        securityDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+        securityDataSource.setUser(env.getProperty("jdbc.user"));
+        securityDataSource.setPassword(env.getProperty("jdbc.password"));
+
+        // set connection pool properties
+        securityDataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
+        securityDataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
+        securityDataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));
+        securityDataSource.setMaxIdleTime(getIntProperty("connection.pool.maxIdleTime"));
+
+        return securityDataSource;
+    }
+
+    // helper method: to read environment property and convert to int
+    private int getIntProperty(String propName) {
+        String propVal = env.getProperty(propName);
+        int intPropVal = Integer.parseInt(propVal);
+        return intPropVal;
+    }
 
     // define a bean for ViewResolver
     @Bean
