4d5728b2e719264752efb37a8a9cd7bee3aa833e
DESKTOP-BL7US23\architbiet <architbiet@gmail.com>
2018-05-05 15:29:32 +0200

    created jdbc user details manager bean

--- src/main/java/com/gemalto/springsecurity/demo/config/DemoSecurityConfig.java
+++ src/main/java/com/gemalto/springsecurity/demo/config/DemoSecurityConfig.java
@@ -1,11 +1,14 @@
 package com.gemalto.springsecurity.demo.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 
 import javax.sql.DataSource;
 
@@ -16,6 +19,21 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
     // add a reference to security data source
     @Autowired
     private DataSource securityDataSource;
+
+    /**
+     * @return - UserDetailsManager
+     * <p>
+     * provides access to database to create user
+     * checks if the user exists
+     * has all of the low-level JDBC code for accessing the security database
+     * will be used in Registration Controller
+     */
+    @Bean
+    public UserDetailsManager userDetailsManager() {
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager();
+        jdbcUserDetailsManager.setDataSource(securityDataSource);
+        return jdbcUserDetailsManager;
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
