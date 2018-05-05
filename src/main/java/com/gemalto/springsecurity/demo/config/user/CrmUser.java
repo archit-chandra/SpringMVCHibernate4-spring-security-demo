32a2c3fb021fb4aae787b8de8a83250e1aef65cd
DESKTOP-BL7US23\architbiet <architbiet@gmail.com>
2018-05-05 15:15:11 +0200

    CrmUser class added, for the registration form

---
+++ src/main/java/com/gemalto/springsecurity/demo/config/user/CrmUser.java
@@ -0,0 +1,39 @@
+package com.gemalto.springsecurity.demo.config.user;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class CrmUser {
+
+    @NotNull(message = "is required")
+    @Size(min = 1, message = "is required")
+    private String userName;
+
+    @NotNull(message = "is required")
+    @Size(min = 1, message = "is required")
+    private String password;
+
+    public CrmUser() {
+    }
+
+    public CrmUser(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
